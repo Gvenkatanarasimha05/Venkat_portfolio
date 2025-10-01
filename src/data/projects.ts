f906c6edd736415c34d0592d19aed8cbe0cd4e23
Gvenkatanarasimha05 <gvenkatnarasimha965@gmail.com>
2025-10-01 15:11:13 +0530

    Add EasyTrip project to portfolio

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -1,8 +1,19 @@
 import { Project } from '../types';
 
 export const projectsData: Project[] = [
+   {
+  id: 1,
+  title: 'EasyTrip',
+  description: 'A full-featured app that helps users plan trips, create personalized itineraries, and manage bookings, while providing smart travel suggestions and real-time updates.',
+  imageUrl: 'src/components/images/travel.jpg',
+  category: 'web',
+  technologies: ['React', 'Node.js', 'Express.js', 'MongoDB'],
+  liveUrl: 'https://easytrip1.vercel.app/',
+  codeUrl: 'https://github.com/Gvenkatanarasimha05/EasyTrip',
+}
+,
   {
-    id: 1,
+    id: 2,
     title: 'FitnessPro',
     description: 'A full-featured app that will help to track the user activation time towards fitness and help to do workouts.',
     imageUrl: 'https://images.pexels.com/photos/1547248/pexels-photo-1547248.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
@@ -11,7 +22,7 @@ export const projectsData: Project[] = [
     codeUrl: 'https://github.com/Gvenkatanarasimha05/fitnesspro',
   },
   {
-    id: 2,
+    id: 3,
     title: 'Website Clones',
     description: 'Website Clones -Spotify',
     imageUrl: 'https://images.pexels.com/photos/218686/pexels-photo-218686.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
@@ -23,7 +34,7 @@ export const projectsData: Project[] = [
 
 
   {
-  id: 3,
+  id: 4,
   title: 'Plant Disease Detection',
   description: 'A deep learning-based web app for detecting plant leaf diseases using image classification. Helps farmers ensure early diagnosis and sustainable agriculture.',
   imageUrl: 'https://www.growag.com/storage/1335/conversions/Go-Micro-testing-crop-1185x680.jpeg',
